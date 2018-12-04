193f46d51ba4a2ae083692f6a09c4d75193811af
jonas27 <joE@12345>
2018-12-04 18:18:37 +0100

    Dont close user when read pump fails

--- impl/client.go
+++ impl/client.go
@@ -72,7 +72,7 @@ func closeTable(c *Client) {
 // reads from this goroutine.
 func (c *Client) readPump() {
 	defer func() {
-		closeUser(c)
+		//closeUser(c)
 	}()
 	c.Conn.SetReadLimit(maxMessageSize)
 	c.Conn.SetReadDeadline(time.Now().Add(pongWait))
