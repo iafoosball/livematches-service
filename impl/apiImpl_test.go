ce66aed96c240a7ad95502a35f750a155040cac7
jonas27 <joE@12345>
2018-12-04 13:38:26 +0100

    test rotatePeople

--- impl/apiImpl_test.go
+++ impl/apiImpl_test.go
@@ -2,15 +2,20 @@ package impl
 
 import (
 	"github.com/iafoosball/livematches-service/models"
+	"log"
 	"testing"
 )
 
 func TestRotatePeople(t *testing.T) {
 	positions := &models.MatchPositions{
-		BlueAttack: "BlueDef",
-		RedAttack:  "RedDef",
-		RedDefense: "RedAtt",
+		BlueAttack: "BlueAtt",
+		RedAttack:  "RedAtt",
+		RedDefense: "RedDef",
 	}
 	rotatePeople(positions)
+	if positions.BlueAttack != "RedAtt" || positions.RedAttack != "RedDef" || positions.RedDefense != "BlueAtt" {
+		log.Println(positions)
+		t.Fail()
+	}
 
 }
