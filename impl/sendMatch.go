20286cce81f7676a8da5d150da5f48d9d472f0bc
jonas27 <joE@12345>
2018-12-11 15:19:45 +0100

    defer connection so it does not crash when matches service is unreachable

--- impl/sendMatch.go
+++ impl/sendMatch.go
@@ -19,18 +19,19 @@ func SendMatch(liveMatch *LiveMatch) {
 	resp, err := http.Post(MatchesAddr+"/matches/", "application/json", bytes.NewReader(js))
 	handleErr(err)
 	if err != nil {
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		handleErr(err)
-		m := models.DocumentMeta{}
-		err = json.NewDecoder(strings.NewReader(string(body))).Decode(&m)
-		handleErr(err)
-		goals := liveMatch.Goals
-		for _, g := range goals {
-			g.From = m.ID
-			g.To = m.ID
-			js, _ = json.Marshal(g)
-			resp, err = http.Post(MatchesAddr+"/goals/", "application/json", bytes.NewReader(js))
-		}
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	handleErr(err)
+	m := models.DocumentMeta{}
+	err = json.NewDecoder(strings.NewReader(string(body))).Decode(&m)
+	handleErr(err)
+	goals := liveMatch.Goals
+	for _, g := range goals {
+		g.From = m.ID
+		g.To = m.ID
+		js, _ = json.Marshal(g)
+		resp, err = http.Post(MatchesAddr+"/goals/", "application/json", bytes.NewReader(js))
 	}
 }
