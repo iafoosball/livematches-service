78ace464d5c888bb717d12b6a63f727b85532c66
jonas27 <joE@12345>
2018-12-11 14:56:16 +0100

    defer connection so it does not crash when matches service is unreachable

--- impl/sendMatch.go
+++ impl/sendMatch.go
@@ -16,22 +16,24 @@ var (
 func SendMatch(liveMatch *LiveMatch) {
 	js, err := json.Marshal(*liveMatch.M)
 	handleErr(err)
-	resp, err := http.Post(MatchesAddr+"/matches/", "application/json", bytes.NewReader(js))
-	handleErr(err)
-	if err != nil {
-
+	defer func() {
+		resp, err := http.Post(MatchesAddr+"/matches/", "application/json", bytes.NewReader(js))
 		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
 		handleErr(err)
-		m := models.DocumentMeta{}
-		err = json.NewDecoder(strings.NewReader(string(body))).Decode(&m)
-		handleErr(err)
-		goals := liveMatch.Goals
-		for _, g := range goals {
-			g.From = m.ID
-			g.To = m.ID
-			js, _ = json.Marshal(g)
-			resp, err = http.Post(MatchesAddr+"/goals/", "application/json", bytes.NewReader(js))
+		if err != nil {
+
+			body, err := ioutil.ReadAll(resp.Body)
+			handleErr(err)
+			m := models.DocumentMeta{}
+			err = json.NewDecoder(strings.NewReader(string(body))).Decode(&m)
+			handleErr(err)
+			goals := liveMatch.Goals
+			for _, g := range goals {
+				g.From = m.ID
+				g.To = m.ID
+				js, _ = json.Marshal(g)
+				resp, err = http.Post(MatchesAddr+"/goals/", "application/json", bytes.NewReader(js))
+			}
 		}
-	}
+	}()
 }
