cad3c207a39b1310257395f95be148a094b6e18f
jonas27 <joE@12345>
2018-12-21 07:24:18 +0100

    Update to tls

--- testclients/testMultiClients/multiClient_test.go
+++ testclients/testMultiClients/multiClient_test.go
@@ -2,7 +2,9 @@ package testMultiClients
 
 import (
 	"github.com/gorilla/websocket"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"net/url"
 	"os"
 	"os/signal"
@@ -15,12 +17,25 @@ var (
 	i int
 )
 
+func TestMultiClientHttp(t *testing.T) {
+	for i := 1; i < 1000000; i++ {
+		log.Println(i)
+		time.Sleep(1 * time.Millisecond)
+		go httpRequ()
+	}
+}
+
+func httpRequ() {
+	r, _ := http.Get("http://iafoosball.me:9000/matches")
+	_, _ = ioutil.ReadAll(r.Body)
+}
+
 func TestMultiClient(t *testing.T) {
 	c := 0
 	for i := 1; i < 100; i++ {
 		for c = 0; c < 100; c++ {
 			go connect()
-			time.Sleep(10 * time.Millisecond)
+			time.Sleep(50 * time.Millisecond)
 		}
 		//time.Sleep(500 * time.Millisecond)
 		log.Println(time.Now())
