8d7c7437503308163b05cc500273dfbf30879c4b
jonas27 <joE@12345>
2018-12-23 20:23:10 +0100

    add tls

--- main/main.go
+++ main/main.go
@@ -67,7 +67,7 @@ func main() {
 		impl.ServeWs(hub, w, r, true, s[2], s[3])
 	})
 	//err := http.ListenAndServe(*host+":"+*port, nil)
-	err := http.ListenAndServeTLS(*host+":"+*port, "cert.pem", "key.pem", nil)
+	err := http.ListenAndServeTLS(*host+":"+*port, "/certs/cert.pem", "/certs/key.pem", nil)
 	//openssl rsa -in key.pem -out key.unencrypted.pem -passin pass:TYPE_YOUR_PASS
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
