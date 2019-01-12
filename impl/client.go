7e5185c5e13f30370cf43425d3a567195db3971a
jonas27 <joE@12345>
2019-01-12 22:59:40 +0100

    ws reconnecting

--- impl/client.go
+++ impl/client.go
@@ -208,6 +208,7 @@ func newClient(id string, hub *Hub, conn *websocket.Conn, isUser bool) *Client {
 
 func tableExists(tableID string, hub *Hub) bool {
 	for c := range hub.clients {
+		log.Println(c.ID)
 		if !c.IsUser && c.Table.ID == tableID {
 			log.Println("table exists")
 			return true
